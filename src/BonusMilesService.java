87faf878a26631424a457d94d098d953f4374e8d
Alex Belkin <dustiklove@yandex.ru>
2023-08-01 20:26:10 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,18 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus = 20;
+        int result;
+        int miles = (price / bonus);{
+
+            return miles;
+        }
+
+    }
+
+    }
+
+
+
+
+
+
