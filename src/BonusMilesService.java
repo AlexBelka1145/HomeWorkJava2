809fd9aa9f9ae8185997ea4904a7bf9f5560fcc6
Alex Belkin <dustiklove@yandex.ru>
2023-08-06 12:40:04 +0300

    files changed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,14 +2,15 @@ public class BonusMilesService {
     public int calculate(int price) {
         int bonus = 20;
         int result;
-        int miles = (price / bonus);{
+        int miles = (price / bonus);
+        {
 
             return miles;
         }
 
     }
 
-    }
+}
 
 
 
